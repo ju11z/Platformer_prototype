c2b083f6181ae83674dd4d52ce6a087ad6411727
studyul <student_yuliya@mail.ru>
2023-10-13 21:22:34 +0300

    fixed player death bug and hammer damage

--- Assets/Scripts/WindBlock.cs
+++ Assets/Scripts/WindBlock.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WindBlock : MonoBehaviour
 {
     public float UpdateWindInterval = 2.0f;
+    public float MinWindForce = 5f;
+    public float MaxWindForce = 10f;
     private float windForce;
     private Vector3 windDirection;
     private void OnCollisionStay(Collision collision)
@@ -27,10 +29,10 @@ public class WindBlock : MonoBehaviour
     {
         //Debug.Log("update win");
 
-        float rX = Random.Range(1, 3);
-        float rZ = Random.Range(1, 3);
+        float rX = Random.Range(MinWindForce, MaxWindForce);
+        float rZ = Random.Range(MinWindForce, MaxWindForce);
 
-        float rF = Random.Range(1, 5);
+        float rF = Random.Range(MinWindForce, MaxWindForce);
 
         windDirection = new Vector3(rX, 0, rZ);
         windForce = rF;
