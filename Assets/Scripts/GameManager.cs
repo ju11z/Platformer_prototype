e9f8af9f291919702f2b593e87ff5a198b704ea6
studyul <student_yuliya@mail.ru>
2023-10-18 16:24:20 +0300

    updated level design

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -63,6 +63,11 @@ public class GameManager : MonoBehaviour
             UIManager.UpdateTimer(minutes, seconds);
             //TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 
     private void SetGameState(state state)
